9acb68f2715242adb649573459ec265388a99e12
Andrian004 <128672158+Andrian004@users.noreply.github.com>
2024-02-07 14:42:28 +0700

    feat: implement tanstack/react-query in login page

--- src/routes/admin/login-page.jsx
+++ src/routes/admin/login-page.jsx
@@ -1,7 +1,8 @@
 import { useState, useEffect } from "react";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, redirect } from "react-router-dom";
 import { useCookies } from "react-cookie";
 import axios from "axios";
+import { useMutation } from "@tanstack/react-query";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 
@@ -17,28 +18,26 @@ const Login = () => {
     if (token) return navigate("/dashboard/home");
   }, [token, navigate]);
 
-  async function handleLogin(e) {
+  const mutation = useMutation({
+    mutationFn: (formData) => {
+      return axios.post("http://localhost:8082/user/login", formData);
+    },
+    onSuccess: (response) => {
+      setCookie("token", response.data.token);
+      return redirect("/dashboard/home");
+    },
+  });
+
+  function handleLogin(e) {
     e.preventDefault();
 
     const formData = { username, password };
-    try {
-      const response = await axios.post(
-        "http://localhost:8082/user/login",
-        formData
-      );
-
-      await setCookie("token", response.data.token);
-
-      if (response.status === 200) navigate("/dashboard/home");
-    } catch (err) {
-      // TODO: Error handling
-      console.log(err);
-    }
+    mutation.mutate(formData);
   }
 
   return (
     <div className="w-full h-screen flex justify-center items-center px-2">
-      <div className="w-full md:w-[700px] h-[400px] flex bg-gray-100 shadow-2xl rounded-3xl">
+      <div className="w-full md:w-[700px] h-max flex bg-gray-100 shadow-2xl rounded-3xl">
         <section className="form w-full md:w-[350px] h-full p-4">
           <form
             className="w-full h-full flex flex-col items-center justify-center gap-4"
@@ -47,6 +46,11 @@ const Login = () => {
             <h2 className="text-center font-bold w-full py-2 text-sky-800">
               Login
             </h2>
+            {mutation.error && (
+              <div className="border border-rose-500 rounded p-1 bg-rose-50 text-center">
+                {mutation.error.response.data.message}
+              </div>
+            )}
             <div className="w-full flex flex-col gap-2">
               <label
                 htmlFor="username"
@@ -85,8 +89,8 @@ const Login = () => {
             <p className="text-sm">Forgot Your Password?</p>
           </form>
         </section>
-        <section className="hero-login hidden md:inline w-[350px] h-full">
-          <div className="w-full h-full flex flex-col justify-center items-center gap-4 bg-sky-800 text-white rounded-l-[150px] rounded-r-3xl">
+        <section className="hero-login hidden md:inline w-[350px] min-h-full">
+          <div className="w-full h-full flex flex-col justify-center items-center gap-4 bg-sky-800 text-white rounded-l-[100px] rounded-r-3xl">
             <h2 className="text-center text-4xl font-bold">Hi, Admin!</h2>
             <p className="text-center">
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi,
