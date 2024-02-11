9e4a4efdc968adff0758ba14a83097bf4b77d7bc
Andrian004 <128672158+Andrian004@users.noreply.github.com>
2024-02-11 21:39:08 +0700

    feat: implement (read & delete) evaluation result

---
+++ src/routes/admin/detail-evaluasi-page.jsx
@@ -0,0 +1,9 @@
+const DetailEvaluasiPage = () => {
+  return (
+    <div className="w-full h-full flex justify-center items-center">
+      <h1 className="text-2xl font-bold">Detail Evaluation Page</h1>
+    </div>
+  );
+};
+
+export default DetailEvaluasiPage;
