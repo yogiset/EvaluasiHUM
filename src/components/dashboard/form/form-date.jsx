859ca42e3a7dba4b0ea6116e6d268045bae1e7cd
Andrian004 <128672158+Andrian004@users.noreply.github.com>
2024-02-20 22:32:00 +0700

    feat: add date picker

---
+++ src/components/dashboard/form/form-date.jsx
@@ -0,0 +1,63 @@
+import { CalendarDays } from "lucide-react";
+import { format } from "date-fns";
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+import { Calendar } from "@/components/ui/calendar";
+import {
+  FormField,
+  FormItem,
+  FormLabel,
+  FormControl,
+  FormMessage,
+} from "@/components/ui/form";
+import {
+  Popover,
+  PopoverTrigger,
+  PopoverContent,
+} from "@/components/ui/popover";
+
+export const FormDate = ({ form, id, label }) => {
+  return (
+    <FormField
+      control={form.control}
+      name={id}
+      render={({ field }) => (
+        <FormItem className="flex flex-col">
+          <FormLabel>{label}</FormLabel>
+          <Popover>
+            <PopoverTrigger asChild>
+              <FormControl>
+                <Button
+                  variant={"outline"}
+                  className={cn(
+                    "w-[240px] pl-3 text-left font-normal",
+                    !field.value && "text-muted-foreground"
+                  )}
+                >
+                  {field.value ? (
+                    format(field.value, "dd MMMM yyyy") // PACKAGE: date-fns
+                  ) : (
+                    <span>Pick a date</span>
+                  )}
+                  <CalendarDays className="ml-auto h-4 w-4 opacity-50" />
+                </Button>
+              </FormControl>
+            </PopoverTrigger>
+            <PopoverContent className="w-auto p-0" align="start">
+              <Calendar
+                mode="single"
+                selected={field.value}
+                onSelect={field.onChange}
+                disabled={(date) =>
+                  date > new Date() || date < new Date("1900-01-01")
+                }
+                initialFocus
+              />
+            </PopoverContent>
+          </Popover>
+          <FormMessage className="text-xs" />
+        </FormItem>
+      )}
+    />
+  );
+};
