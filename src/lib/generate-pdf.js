9484c0e3a8068d8e1e07caede6b39d171bf4710f
yogiset <the3verlastin9@gmail.com>
2024-03-09 01:08:29 +0700

    improve pdf

--- src/lib/generate-pdf.js
+++ src/lib/generate-pdf.js
@@ -6,6 +6,7 @@ export function generatePdf({
   nama,
   divisi,
   jabatan,
+  email,
   tanggal_masuk,
   masa_kerja,
   tingkatan,
@@ -28,7 +29,7 @@ export function generatePdf({
   );
 
   // User info
-  doc.setFontSize(12);
+  doc.setFontSize(14).setFont(undefined, "bold");
   doc.text("NIK: " + nik, 10, 80, {
     maxWidth: doc.internal.pageSize.width - 10,
   });
@@ -41,33 +42,36 @@ export function generatePdf({
   doc.text("Jabatan: " + jabatan, 10, 101, {
     maxWidth: doc.internal.pageSize.width - 10,
   });
-  doc.text("Tanggal Masuk: " + tanggal_masuk, 10, 108, {
+  doc.text("Email: " + email, 10, 108, {
     maxWidth: doc.internal.pageSize.width - 10,
   });
-  doc.text("Masa kerja: " + masa_kerja, 10, 115, {
+  doc.text("Tanggal Masuk: " + tanggal_masuk, 10, 115, {
     maxWidth: doc.internal.pageSize.width - 10,
   });
-  doc.text("Tingkatan: " + tingkatan, 10, 122, {
+  doc.text("Masa kerja: " + masa_kerja, 10, 122, {
     maxWidth: doc.internal.pageSize.width - 10,
   });
-  doc.text("Tanggal Evaluasi: " + tanggal_evaluasi, 10, 129, {
+  doc.text("Tingkatan: " + tingkatan, 10, 129, {
+    maxWidth: doc.internal.pageSize.width - 10,
+  });
+  doc.text("Tanggal Evaluasi: " + tanggal_evaluasi, 10, 136, {
     maxWidth: doc.internal.pageSize.width - 10,
   });
 
   // Evaluation result
   doc.setFontSize(14).setFont(undefined, "bold");
-  doc.text("Hasil evaluasi:", 10, 143);
+  doc.text("Hasil evaluasi:", 10, 150);
   doc.setFontSize(12).setFont(undefined, "normal");
-  doc.text(hasilEvaluasi, 10, 150, {
+  doc.text(hasilEvaluasi, 10, 157, {
     maxWidth: doc.internal.pageSize.width - 10,
     lineHeightFactor: 1.5,
   });
 
   // Improvement
   doc.setFontSize(14).setFont(undefined, "bold");
-  doc.text("Perlu ditingkatkan:", 10, 164);
+  doc.text("Perlu ditingkatkan:", 10, 171);
   doc.setFontSize(12).setFont(undefined, "normal");
-  doc.text(perluDitingkatkan, 10, 171, {
+  doc.text(perluDitingkatkan, 10, 178, {
     maxWidth: doc.internal.pageSize.width - 20,
     lineHeightFactor: 1.5,
   });
