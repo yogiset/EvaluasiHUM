3dbda9dc8045339fedf28495b4fed692dee97ca9
yogiset <the3verlastin9@gmail.com>
2024-03-08 18:20:36 +0700

    add divisi,tgl masuk,masa kerja,tingkatan

--- src/lib/generate-pdf.js
+++ src/lib/generate-pdf.js
@@ -4,8 +4,12 @@ import mainBg from "@/assets/pdf-bg.png";
 export function generatePdf({
   nik,
   nama,
+  divisi,
   jabatan,
-  tanggal,
+  tanggal_masuk,
+  masa_kerja,
+  tingkatan,
+  tanggal_evaluasi,
   hasilEvaluasi,
   perluDitingkatkan,
 }) {
@@ -31,27 +35,39 @@ export function generatePdf({
   doc.text("Nama: " + nama, 10, 87, {
     maxWidth: doc.internal.pageSize.width - 10,
   });
-  doc.text("Jabatan: " + jabatan, 10, 94, {
+  doc.text("Divisi: " + divisi, 10, 94, {
     maxWidth: doc.internal.pageSize.width - 10,
   });
-  doc.text("Tanggal: " + tanggal, 10, 101, {
+  doc.text("Jabatan: " + jabatan, 10, 101, {
+    maxWidth: doc.internal.pageSize.width - 10,
+  });
+  doc.text("Tanggal Masuk: " + tanggal_masuk, 10, 108, {
+    maxWidth: doc.internal.pageSize.width - 10,
+  });
+  doc.text("Masa kerja: " + masa_kerja, 10, 115, {
+    maxWidth: doc.internal.pageSize.width - 10,
+  });
+  doc.text("Tingkatan: " + tingkatan, 10, 122, {
+    maxWidth: doc.internal.pageSize.width - 10,
+  });
+  doc.text("Tanggal Evaluasi: " + tanggal_evaluasi, 10, 129, {
     maxWidth: doc.internal.pageSize.width - 10,
   });
 
   // Evaluation result
   doc.setFontSize(14).setFont(undefined, "bold");
-  doc.text("Hasil evaluasi:", 10, 115);
+  doc.text("Hasil evaluasi:", 10, 143);
   doc.setFontSize(12).setFont(undefined, "normal");
-  doc.text(hasilEvaluasi, 10, 122, {
+  doc.text(hasilEvaluasi, 10, 150, {
     maxWidth: doc.internal.pageSize.width - 10,
     lineHeightFactor: 1.5,
   });
 
   // Improvement
   doc.setFontSize(14).setFont(undefined, "bold");
-  doc.text("Perlu ditingkatkan:", 10, 136);
+  doc.text("Perlu ditingkatkan:", 10, 164);
   doc.setFontSize(12).setFont(undefined, "normal");
-  doc.text(perluDitingkatkan, 10, 143, {
+  doc.text(perluDitingkatkan, 10, 171, {
     maxWidth: doc.internal.pageSize.width - 20,
     lineHeightFactor: 1.5,
   });
