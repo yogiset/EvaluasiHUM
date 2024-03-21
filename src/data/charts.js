c91b39d17639e2db893f8ad3e3cc645e37e51fe5
yogiset <the3verlastin9@gmail.com>
2024-03-21 14:26:33 +0700

    feat: add charts

---
+++ src/data/charts.js
@@ -0,0 +1,95 @@
+export const chartsData = [
+  {
+    label: "Baik",
+    data: [
+      {
+        primary: "2023-09-01T00:00:00.000Z",
+        result: 11,
+      },
+      {
+        primary: "2023-10-01T00:00:00.000Z",
+        result: 53,
+      },
+      {
+        primary: "2023-11-01T00:00:00.000Z",
+        result: 230,
+      },
+      {
+        primary: "2023-12-01T00:00:00.000Z",
+        result: 256,
+      },
+      {
+        primary: "2024-01-01T00:00:00.000Z",
+        result: 279,
+      },
+      {
+        primary: "2024-02-01T00:00:00.000Z",
+        result: 785,
+      },
+    ],
+    currentResult: 785,
+    color: "#3ca139",
+  },
+  {
+    label: "Sedang",
+    data: [
+      {
+        primary: "2023-09-01T00:00:00.000Z",
+        result: 2,
+      },
+      {
+        primary: "2023-10-01T00:00:00.000Z",
+        result: 11,
+      },
+      {
+        primary: "2023-11-01T00:00:00.000Z",
+        result: 12,
+      },
+      {
+        primary: "2023-12-01T00:00:00.000Z",
+        result: 27,
+      },
+      {
+        primary: "2024-01-01T00:00:00.000Z",
+        result: 16,
+      },
+      {
+        primary: "2024-02-01T00:00:00.000Z",
+        result: 328,
+      },
+    ],
+    currentResult: 328,
+    color: "#ffcf00",
+  },
+  {
+    label: "Kurang baik",
+    data: [
+      {
+        primary: "2023-09-01T00:00:00.000Z",
+        result: 6,
+      },
+      {
+        primary: "2023-10-01T00:00:00.000Z",
+        result: 15,
+      },
+      {
+        primary: "2023-11-01T00:00:00.000Z",
+        result: 79,
+      },
+      {
+        primary: "2023-12-01T00:00:00.000Z",
+        result: 12,
+      },
+      {
+        primary: "2024-01-01T00:00:00.000Z",
+        result: 259,
+      },
+      {
+        primary: "2024-02-01T00:00:00.000Z",
+        result: 41,
+      },
+    ],
+    currentResult: 41,
+    color: "#ff0000",
+  },
+];
