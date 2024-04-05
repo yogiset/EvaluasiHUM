d4ae739c0d73fce9e3bc8b515b6e8346472ebe15
yogiset <the3verlastin9@gmail.com>
2024-04-05 13:51:54 +0700

    create himpunan kriteria schema

---
+++ src/schema/himpunan-kriteria-schema.js
@@ -0,0 +1,27 @@
+import { z } from "zod";
+
+export const himpunankriteriaSchema = z.object({
+  nmkriteria: z
+    .string({
+      required_error: "nama kriteria is required!",
+      invalid_type_error: "nama kriteria is required!",
+    })
+    .min(4, { message: "nama kriteria must be at least 4 characters." }),
+  nmhimpunan: z
+    .string({
+      required_error: "Nama himpunan is required!",
+      invalid_type_error: "Nama himpunan is required!",
+    })
+    .min(4, { message: "Nama himpunan must be at least 4 characters." }),
+  nilai: z.coerce
+    .number({
+      required_error: "Please fill this field!",
+    })
+    .nonnegative({ message: "Nilai must be a positive value." }),  
+  keterangan: z
+    .string({
+      required_error: "Keterangan himpunan is required!",
+      invalid_type_error: "Keterangan himpunan is required!",
+    })
+    .min(4, { message: "Keterangan must be at least 4 characters." }),
+});
