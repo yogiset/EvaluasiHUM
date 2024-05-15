b0feb1e84d82dcd839a74cd19d1fc34e222cfd43
yogiset <the3verlastin9@gmail.com>
2024-05-15 11:02:46 +0700

    added more configurations

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,14 @@ import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host:"0.0.0.0",
+    port:"5173",
+  },
+  preview:{
+    host:"0.0.0.0",
+    port:"3001",
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
