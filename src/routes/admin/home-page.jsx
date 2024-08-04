426f092bc0d008fc1c03cd5bfc6893eeaa2b55b8
Yoghi Setiawan <the3verlastin9@gmail.com>
2024-08-04 11:18:17 +0700

    hide routing for evaluasi

--- src/routes/admin/home-page.jsx
+++ src/routes/admin/home-page.jsx
@@ -124,7 +124,7 @@ const BoardList = ({ role }) => {
   const allBoards = [
     // { title: "Test Evaluasi", path: "/evaluasi/user-form", bg: "bg-black-400" },
     { title: "Data Sales", path: "/dashboard/sales", bg: "bg-blue-400" },
-    { title: "Evaluasi", path: "/dashboard/evaluasi", bg: "bg-rose-400" },
+    // { title: "Evaluasi", path: "/dashboard/evaluasi", bg: "bg-rose-400" },
     { title: "Karyawan", path: "/dashboard/karyawan", bg: "bg-sky-500" },
     { title: "Pertanyaan", path: "/dashboard/pertanyaan", bg: "bg-orange-500" },
     { title: "Picos", path: "/dashboard/picos", bg: "bg-yellow-500" },
